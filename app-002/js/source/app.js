dc4124538988b6b9849aca88b985758404585bc9
Александр <achechel@elar.ru>
2017-05-12 17:49:44 +0300

    add Component Rating add Component Suggest

--- app-002/js/source/app.js
+++ app-002/js/source/app.js
@@ -6,6 +6,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from "./components/Button";
 import Suggest from "./components/Suggest";
+import Rating from "./components/Rating";
 
 let headers = localStorage.getItem('headers');
 let data = localStorage.getItem('data');
@@ -32,6 +33,21 @@ ReactDOM.render(
         <div>
             <h2>Suggestion</h2>
             <Suggest options={['eenie', 'mennie', 'mine', 'mo']} />
+        </div>
+        <div>
+            <h2>Rating</h2>
+            <div>
+                No initial value: <Rating />
+            </div>
+            <div>
+                Initial value 4: <Rating defaultValue={4} />
+            </div>
+            <div>
+                This one goes to 11: <Rating max={11} />
+            </div>
+            <div>
+                Read-only: <Rating readonly={true} defaultValue={4}/>
+            </div>
         </div>
     </div>,
     document.getElementById('pad')
---
+++ app-002/js/source/components/Rating.js
@@ -0,0 +1,76 @@
+/**
+ * Created by AChechel on 12.05.2017.
+ */
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+class Rating extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            rating: props.defaultValue,
+            tmpRating: props.defaultValue
+        };
+    }
+
+    getValue() {
+        return this.state.rating;
+    }
+
+    setTemp(rating) {
+        this.setState({
+            tmpRating: rating
+        });
+    }
+
+    setRating(rating) {
+        this.setState({
+            tmpRating: rating,
+            rating: rating
+        });
+    }
+
+    reset(){
+        this.setTemp(this.state.rating);
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.setRating(nextProps.defaultValue);
+    }
+
+    render() {
+        const stars = [];
+        for (let i = 1; i <= this.pros.max; i++) {
+            stars.push(<span
+                className={i <= this.state.tmpRating ? 'RatingOn' : null}
+                key={i}
+                onClick={!this.props.readonly && this.setTemp.bind(this, i)}
+                onMouseOver={!this.props.readonly && this.setTemp.bind(this, i)}
+            >&#9734</span>);
+        }
+        return (<div
+            className={classNames({Rating:true,RatingReadonly:this.props.readonly})}
+            onMouseOut={this.reset.bind(this)}
+        >
+
+            {stars}
+            {this.props.readonly || !this.props.id ? null : <input type="hidden" id={this.props.id} value={this.state.rating} />}
+        </div>);
+    }
+}
+
+Rating.propTypes = {
+    defaultValue: PropTypes.number,
+    readonly: PropTypes.bool,
+    max: PropTypes.number
+};
+
+Rating.deafultType = {
+    defaultValue: 0,
+    max: 5
+};
+
+
+export default Rating
